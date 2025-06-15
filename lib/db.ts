e4d40c2245ad510c9f3a0f40a72615ce2b7f2e08
Nicolas-ruiz15 <107676963+Nicolas-ruiz15@users.noreply.github.com>
2025-06-15 13:19:38 +0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'usr_sanando_desde_el_ser',
-  password: process.env.DB_PASSWORD || '@deKA5d1aTju9@am',
-  database: process.env.DB_NAME || 'sanando_desde_el_ser',
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
